49bd43beedcb63e12a5eaf6dd4172bbeafd3d36c
mfrancis95 <mikefrancis95@gmail.com>
2016-09-04 11:25:04 -0400

    Add Bitmap fill() method and an optional Bitmap fallback for handling missing textures

--- src/com/amf/jaycaster/Bitmap.java
+++ src/com/amf/jaycaster/Bitmap.java
@@ -7,10 +7,13 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import javax.imageio.ImageIO;
 
 public class Bitmap {
+    
+    public static Bitmap fallback;
     
     private static final HashMap<String, Bitmap> bitmaps;
     
@@ -19,7 +22,11 @@ public class Bitmap {
     }
     
     public static Bitmap get(String name) {
-        return bitmaps.get(name);
+        Bitmap bitmap = bitmaps.get(name);
+        if (bitmap == null) {
+            bitmap = fallback;
+        }
+        return bitmap;
     }
     
     private static void load(URL url) throws IOException, URISyntaxException {
@@ -60,6 +67,10 @@ public class Bitmap {
         this.width = width;
         this.height = height;
         this.pixels = pixels;
+    }
+    
+    public void fill(int color) {
+        Arrays.fill(pixels, color);
     }
     
     public int getPixel(int x, int y) {
